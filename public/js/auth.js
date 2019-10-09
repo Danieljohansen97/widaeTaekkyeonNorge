5ccb276c1f6630312c8cb218d33cabf732eb461a
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-10-09 14:24:33 +0200

    Add admin page

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,15 +1,16 @@
 // add admin cloud function
 const adminForm = document.querySelector('.admin-actions');
-adminForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+if (adminForm) {
+    adminForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const adminEmail = document.querySelector('#admin-email').value;
-    const addAdminRole = functions.httpsCallable('addAdminRole');
-    addAdminRole({ email: adminEmail }).then(result => {
-        console.log(result);
+        const adminEmail = document.querySelector('#admin-email').value;
+        const addAdminRole = functions.httpsCallable('addAdminRole');
+        addAdminRole({ email: adminEmail }).then(result => {
+            console.log(result);
+        });
     });
-});
-
+}
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
     if (user) {
